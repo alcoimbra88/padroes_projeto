b4417f0e691dbd55dc1c852086bc7c91c5c1e2fb
alcoimbra88 <al_coimbra@hotmail.com>
2024-07-25 01:37:54 -0300

    add: factory method

---
+++ src/creational/factory-method/client-code.ts
@@ -0,0 +1,17 @@
+import { CarFactory } from "./factories/car-factory";
+import { randomCarAlgorithm } from "./main/random-vehicle-algorithm";
+import { randomNumbers } from "./utils/random-numbers";
+
+const carFactory = new CarFactory()
+const customerNames = ['Alan','Henrique','Lina','Omar']
+
+for ( let i = 0 ; i < 10; i ++){
+    const vehicle = randomCarAlgorithm()
+    const name = customerNames[randomNumbers(customerNames.length)]
+    vehicle.pickUp(name)
+    vehicle.stop()
+
+    const newCar = carFactory.pickUp(name, `Novo Carro - ${randomNumbers(100)}`)
+    newCar.stop()
+    console.log('------')
+}
